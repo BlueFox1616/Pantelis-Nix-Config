f2e3ee36439df6915a02e549fa9d6052fd834efa
BlueFox1616 <pantvch@gmail.com>
2024-12-14 16:14:36 +0200

    Scripted auto-commit on change (2024-12-14 16:14:36) by gitwatch.sh

--- modules/git.nix
+++ modules/git.nix
@@ -1,16 +1,31 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: {
+  # Configure the git user with a restricted shell and home directory
+  users.users.git = {
+    isSystemUser = true;  # Create the git user as a system user
+    group = "git";        # Assign to git group
+    home = "/var/lib/git-server";  # Path to store git repositories
+    createHome = true;    # Create home directory if it doesn't exist
+    shell = "${pkgs.git}/bin/git-shell";  # Set the login shell to git-shell
+    openssh.authorizedKeys.keys = [
+      # SSH public keys of authorized users
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF38sHxXn/r7KzWL1BVCqcKqmZA/V76N/y5p52UQghw7 example"
+    ];
+  };
 
-{
-  # Ensure Git is installed
-  environment.systemPackages = with pkgs; [
-    git
-  ];
+  # Create the git group if it doesn't exist
+  users.groups.git = {};
 
-  # Set Git configuration using a wrapper to customize the user name and email
-  environment.etc."gitconfig".text = ''
-    [user]
-      name = BlueFox1616
-      email = pantvch@gmail.com
-  '';
+  # Enable SSH service and set restrictions for the git user
+  services.openssh = {
+    enable = true;
+    extraConfig = ''
+      Match user git
+        AllowTcpForwarding no
+        AllowAgentForwarding no
+        PasswordAuthentication no
+        PermitTTY no
+        X11Forwarding no
+    '';
+  };
 }
 
