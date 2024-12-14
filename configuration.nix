fa4d6b3ac84e518f98cc0a77b97036ff28e8e641
BlueFox1616 <pantvch@gmail.com>
2024-12-14 16:02:52 +0200

    Scripted auto-commit on change (2024-12-14 16:02:52) by gitwatch.sh

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ inputs, ... }:
+{ pkgs, inputs, ... }:  # Import pkgs to access package set
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
       ./packages.nix
       ./modules/bundle.nix
@@ -18,22 +14,17 @@
   nix.settings.auto-optimise-store = true;
   programs.dconf.enable = true;
 
-
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Enable networking
   networking.networkmanager.enable = true;
 
-
   # Set your time zone.
   time.timeZone = "Europe/Athens";
-  
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "el_GR.UTF-8";
     LC_IDENTIFICATION = "el_GR.UTF-8";
@@ -49,25 +40,22 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-
   system.stateVersion = "24.05"; # Did you read the comment?
-  # In /etc/nixos/configuration.nix
 
-# Enable SSH
-services.openssh.enable = true;
+  # Enable SSH and SSH agent
+  services.openssh.enable = true;
+  
+  # Configure your user
+  users.users.pantelis = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];  # Modify based on your user setup
+    shell = pkgs.zsh;  # Change to your preferred shell if needed
+    sshKeys = [
+      "/home/pantelis/.ssh/id_rsa.pub" # Path to your public SSH key
+    ];
+  };
 
-# Add SSH key to the SSH agent
-users.users.pantelis = {
-  isNormalUser = true;
-  extraGroups = [ "wheel" ]; # Modify based on your user setup
-  shell = pkgs.zsh; # Modify your preferred shell if needed
-  sshKeys = [
-    "/home/pantelis/.ssh/id_rsa" # Path to your private SSH key
-  ];
-};
-
-# You can also configure a key for GitHub:
-programs.ssh.startAgent = true;
-
+  # Start SSH agent for key management
+  programs.ssh.startAgent = true;
 }
 
