0cd01017ac29d0440d77740efa75bbec696e5c91
System administrator <pantvch@gmail.com>
2024-12-14 15:55:53 +0200

    Scripted auto-commit on change (2024-12-14 15:55:53) by gitwatch.sh

--- modules/git.nix
+++ modules/git.nix
@@ -1,31 +1,16 @@
-# git.nix - Git configuration for NixOS
-
 { config, pkgs, ... }:
 
-let
-  # Define your Git user details here
-  gitUserName = "BlueFox1616";
-  gitUserEmail = "pantvch@gmail.com";
-in {
+{
+  # Ensure Git is installed
+  environment.systemPackages = with pkgs; [
+    git
+  ];
 
-  # Configure Git
-  git = {
-    userName = "BlueFox1616";
-    userEmail = "pantvch@gmail.com";
-  };
-
-  # Enable SSH for GitHub access (ensure the correct SSH key is available)
-  programs.ssh = {
-    enable = true;
-  };
-
-  # Optionally, ensure Git is configured as safe for the repository path
-  systemd.services.gitwatch = {
-    enable = true;
-    path = "/etc/nixos";  # Adjust with the path to your repository
-    remote = "git@github.com:BlueFox1616/Pantelis-Nix-Config.git";  # Replace with your repository URL
-    user = "root";  # The user to run the service under
-  };
-
+  # Set Git configuration using a wrapper to customize the user name and email
+  environment.etc."gitconfig".text = ''
+    [user]
+      name = BlueFox1616
+      email = pantvch@gmail.com
+  '';
 }
 
