e22d3f2ed7e343d69a245518bef9b37be7561619
Pantelis <pantvch@gmail.com>
2024-12-15 19:09:21 +0200

    Scripted auto-commit on change (2024-12-15 19:09:21) by gitwatch.sh

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -1,5 +1,11 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
- 
+
+  systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+    editor = false;
+  };
 }
+
