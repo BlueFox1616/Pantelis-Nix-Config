b6962b688d596e90a3da88cd377a7d663f9def22
Pantelis <pantvch@gmail.com>
2024-12-18 20:40:54 +0200

    Scripted auto-commit on change (2024-12-18 20:40:54) by gitwatch.sh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,11 +6,14 @@
     homeDirectory = "/home/pantelis";
     stateVersion = "23.11";
 
+    # Import the bundle.nix
+    imports = [
+      ./modules/bundle.nix
+    ];
+
     packages = with pkgs; [
       neofetch
     ];
-
-    };
   };
 }
 
