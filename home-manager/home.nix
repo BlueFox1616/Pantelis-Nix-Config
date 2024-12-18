29c601ade7d1b9f3ddd3650f49ff3d9b0ca39a80
Pantelis <pantvch@gmail.com>
2024-12-18 21:25:58 +0200

    Scripted auto-commit on change (2024-12-18 21:25:58) by gitwatch.sh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,36 +1,12 @@
 { pkgs, config, ... }: {
 
   imports = [
-    ./home.nix
+    ./bundle.nix
   ];
 
   home-manager.users.pantelis = {
     homeDirectory = "/home/pantelis";
     stateVersion = "23.11";
-
-    packages = with pkgs; [
-      neofetch
-      # Add any other packages here if needed
-    ];
-
-    gtk = {
-      enable = true;
-      theme = {
-        name = "WhiteSur";
-        package = pkgs.whitesur-gtk-theme;
-      };
-    };
-  };
-
-  # If you have other users, you can define them here similarly
-  home-manager.users.pantelis = {
-    homeDirectory = "/home/pantelis";
-    stateVersion = "23.11";
-
-    packages = with pkgs; [
-      neofetch
-      # Add any other packages here if needed
-    ];
 
     gtk = {
       enable = true;
