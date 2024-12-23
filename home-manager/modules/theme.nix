b4f60418fcf7751434cbd4ba196d1172265c86a5
Pantelis <pantvch@gmail.com>
2024-12-23 13:27:18 +0200

    Scripted auto-commit on change (2024-12-23 13:27:18) by gitwatch.sh

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -22,7 +22,7 @@
 
   home.activation = {
     installThemes = lib.hm.dag.entryAfter [ "writeBoundary" "installPackages" "git" ] ''
-      ${pkgs.git}/bin/git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git 
+      ${pkgs.git}/bin/git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 
       theme_dir="$HOME/WhiteSur-gtk-theme"
       install_script="$theme_dir/install.sh"
@@ -33,7 +33,7 @@
         chmod +x "$install_script"
         export PATH=${pkgs.sassc}/bin:${pkgs.which}/bin:${pkgs.getent}/bin:${pkgs.util-linux}/bin:${pkgs.glib.dev}/bin:${pkgs.libxml2.bin}/bin:${pkgs.sudo}/bin:$PATH
         cd "$theme_dir"
-        ${pkgs.bash}/bin/bash -c "bash $install_script -l" 
+        ${pkgs.bash}/bin/bash -c "./install.sh -l" 
         rm -rf "$theme_dir"
       else
         echo "Theme installation script not found at $install_script"
