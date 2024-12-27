7a9e882d21e06d929b0653deeb539955ceebe423
Pantelis <pantvch@gmail.com>
2024-12-27 16:36:44 +0200

    Scripted auto-commit on change (2024-12-27 16:36:44) by gitwatch.sh

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     nixcord.url = "github:kaylorben/nixcord";
 
