c868da9005503aaf9316fbe1ecddee4d8ddf14e6
Pantelis <pantvch@gmail.com>
2024-12-31 00:03:35 +0200

    Scripted auto-commit on change (2024-12-31 00:03:35) by gitwatch.sh

---
+++ modules/upgrade.nix
@@ -0,0 +1,17 @@
+{ config, inputs, ... }:
+
+{
+  # Enable automatic system upgrades
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # Print build logs
+    ];
+    dates = "02:00"; # Scheduled time for upgrades
+    randomizedDelaySec = "45min"; # Randomized delay to avoid simultaneous updates
+  };
+}
+
