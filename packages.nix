c80b52b9b7d51a8c58d3df658c5a8ea1c4c18ca9
Pantelis <pantvch@gmail.com>
2024-12-31 13:49:08 +0200

    Scripted auto-commit on change (2024-12-31 13:49:08) by gitwatch.sh

--- packages.nix
+++ packages.nix
@@ -34,7 +34,8 @@
     #pkgs.bottles
     pkgs.libreoffice-qt6-fresh
     pkgs.git
-    inputs.zen-browser.packages."${system}".default
+    inputs.zen-browser.packages."${system}".beta
+    inputs.zen-browser.packages."${system}".twilight
     pkgs.localsend
     pkgs.qbittorrent
     pkgs.lutris
